88a5e102c0071157a1b0c7da764fc593f4a3d148
kborzecki <konradsborzecki@gmail.com>
2017-10-17 18:01:47 +0200

    add project dist

--- src/Services/Downloader.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default class Downloader{
-  static getFile(url){
-    return axios.get(url, { headers: { 'Accept': 'application/xml' } })
-  }
-}
--- src/Services/Exchanger.js
+++
@@ -1,7 +0,0 @@
-export default class Exchanger {
-  static convert(value, currencyFrom, currencyTo){
-    console.log(currencyTo)
-    console.log(`${value} * ${currencyFrom.getValue()} / ${currencyTo.getValue()} = ${value * currencyFrom.getValue() / currencyTo.getValue()}`)
-    return value * currencyFrom.getValue() / currencyTo.getValue()
-  }
-}
